57c69ec0887d85304c1650c8e871288cf8304db8
Dominic Evans <dominic.evans@uk.ibm.com>
2017-04-10 14:23:58 +0100

    Add LIBDIR var for travis build differences

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -5,12 +5,14 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   brew install node cmake coreutils findutils
   CP=gcp
   FIND=gfind
+  LIBDIR=lib64
 else
   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
   sudo apt-get update -q
   sudo apt-get install -qqy nodejs cmake g++-4.8 libssl-dev libsasl2-dev sasl2-bin
   CP=cp
   FIND=find
+  LIBDIR=lib
 fi
 
 git clone --depth=1 \
@@ -22,11 +24,11 @@ cd ~/.local/src/qpid-proton \
            -DCMAKE_BUILD_TYPE=RelWithDebInfo \
            -DCMAKE_INSTALL_PREFIX=${HOME}/.local .. \
   && cmake --build . --target install
-for F in $(${FIND} ~/.local/lib* -maxdepth 1 -type l); do
-  ${CP} --remove-destination ~/.local/lib/$(readlink $F) $F
+for F in $(${FIND} ~/.local/${LIBDIR} -maxdepth 1 -type l); do
+  ${CP} --remove-destination ~/.local/${LIBDIR}/$(readlink $F) $F
 done
 ${CP} -R ~/.local/include ${TRAVIS_BUILD_DIR}/include
-${CP} -R ~/.local/lib*/* ${TRAVIS_BUILD_DIR}/lib/ 
+${CP} -R ~/.local/${LIBDIR}/* ${TRAVIS_BUILD_DIR}/lib/ 
 cd ${TRAVIS_BUILD_DIR} \
   && mkdir -p node_modules \
   && npm install core-js request js-yaml
